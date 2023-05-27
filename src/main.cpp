b0d5a13df328ad3562f8d662692e65f0deae6aa6
Deivid Gabriel <deividgabrielproc@gmail.com>
2023-05-27 17:14:36 -0300

    Added example main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
+#include <algorithm>
 #include <iostream>
 
+#include "LeftRightParser.hpp"
+#include "SimpleGrammarLKT.hpp"
+
 int main() {
-    return 0;
+  LeftRightParser *lr = new LeftRightParser(new SimpleGrammarLKT());
+  std::string in;
+
+  int space_counter;
+  int size_after_input;
+
+  std::cout << "Type the string inside the gramatical rule: ";
+  std::getline(std::cin, in);
+
+  size_after_input = in.length();
+
+  // Removing any extra space
+  space_counter = std::count(in.begin(), in.end(), ' ');
+  std::remove(in.begin(), in.end(), ' ');
+  in.resize(size_after_input - space_counter);
+
+  std::cout << lr->parse(in) << "\n";
+
+  delete lr;
+
+  return 0;
 }
